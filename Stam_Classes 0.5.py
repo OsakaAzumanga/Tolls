7c204496816799bb5a2cfd2962ed85e9581f8006
StamGl <162900080+StamGl@users.noreply.github.com>
2024-04-06 15:47:46 +0300

    Update Stam_Classes 0.5.py
    Αλλαγές:
     1) στα ονόματα μεταβλητών ώστε να ταιριάζουν με αυτά που τραβάει η main της Ελένης
    2) Ορθή γραφή στις init των κλάσεων ('','' αντί για ''.'')

--- Stam_Classes 0.5.py
+++ Stam_Classes 0.5.py
@@ -2,33 +2,33 @@ import random
 
 class Oxima():
     """ Αυτή είναι η γονική κλάση όλων των οχημάτων"""
-    def __init__(self,arithmos_kykloforias,eidos_oximatos)
-    self.arithmos_kykloforias=arithmos_kykloforias
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
+    self.arKykloforias=arKykloforias
     self.eidos_oximatos=eidos_oximatos #το είδος οχήματος κληρονομείται σε όλες τις υποκλάσεις για αυτό το κρατάω στην γονική - είναι κοινό χαρακτηριστικό όλων των οχημάτων#
-    self.kwdikos=kwdikos #Αναφέρομαι στον κωδικό της εκάστοτε ePass
+    self.kwdkartas=kwdkartas #Αναφέρομαι στον κωδικό της εκάστοτε ePass
 class Epivatiko(Oxima):
     """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self.arithmos_kykloforias,eidos_oximatos,kwdikos)
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
 
     def xrewnei(self):
         return 1.50 #Χρέωση διαφορετική σε κάθε όχημα για να υπολογίζεται το εκάστοτε κόστος διέλευσης
 
 class dikyklo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self.arithmos_kykloforias,eidos_oximatos,kwdikos)
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
 
     def xrewnei(self):
         return 0.60
 
 class Fortigo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self.arithmos_kykloforias,eidos_oximatos,kwdikos)
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
     def xrewnei(self):
         return 3.20
     
 class ePass():
-    def __init__(self.kwdikos,oxima)
-    self.kwdikos=kwdikos
+    def __init__(self,kwdkartas,oxima,ypoloipoLogariasmou,arithmos_dieleysewn)
+    self.kwdkartas=kwdkartas
     self.oxima=oxima
-    self.ypoloipologariasmou=random.uniform(1,5)
+    self.ypoloipoLogariasmou=random.uniform(1,5)
     self.arithmos_dieleysewn=0
