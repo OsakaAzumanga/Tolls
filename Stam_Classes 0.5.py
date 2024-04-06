5d94808cfb90a6bb994866e22e3f5e26f6b49057
StamGl <162900080+StamGl@users.noreply.github.com>
2024-04-06 16:18:57 +0300

    Update Stam_Classes 0.5.py
    προσθήκη παράκαμψης (override) για αντλήση χαρακτηριστικών από constructor και διορθώσεις σε τυπογραφικά. Διόρθωση indentation

--- Stam_Classes 0.5.py
+++ Stam_Classes 0.5.py
@@ -2,33 +2,37 @@ import random
 
 class Oxima():
     """ Αυτή είναι η γονική κλάση όλων των οχημάτων"""
-    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
-    self.arKykloforias=arKykloforias
-    self.eidos_oximatos=eidos_oximatos #το είδος οχήματος κληρονομείται σε όλες τις υποκλάσεις για αυτό το κρατάω στην γονική - είναι κοινό χαρακτηριστικό όλων των οχημάτων#
-    self.kwdkartas=kwdkartas #Αναφέρομαι στον κωδικό της εκάστοτε ePass
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas):
+        self.arKykloforias=arKykloforias
+        self.eidos_oximatos=eidos_oximatos #το είδος οχήματος κληρονομείται σε όλες τις υποκλάσεις για αυτό το κρατάω στην γονική - είναι κοινό χαρακτηριστικό όλων των οχημάτων#
+        self.kwdkartas=kwdkartas #Αναφέρομαι στον κωδικό της εκάστοτε ePass
 class Epivatiko(Oxima):
     """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
-
+    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas):
+        super().__init__(arKykloforias,eidos_oximatos,kwdkartas)
+    
     def xrewnei(self):
         return 1.50 #Χρέωση διαφορετική σε κάθε όχημα για να υπολογίζεται το εκάστοτε κόστος διέλευσης
 
 class dikyklo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
-
-    def xrewnei(self):
-        return 0.60
+        def __init__(self,arKykloforias,eidos_oximatos,kwdkartas):
+              super().__init__(arKykloforias,eidos_oximatos,kwdkartas)
+        
+        def xrewnei(self):
+            return 0.60
 
 class Fortigo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self,arKykloforias,eidos_oximatos,kwdkartas)
-    def xrewnei(self):
-        return 3.20
+        def __init__(self,arKykloforias,eidos_oximatos,kwdkartas):
+            super().__init__(arKykloforias,eidos_oximatos,kwdkartas)
+        
+        def xrewnei(self):
+            return 3.20
     
 class ePass():
-    def __init__(self,kwdkartas,oxima,ypoloipoLogariasmou,arithmos_dieleysewn)
-    self.kwdkartas=kwdkartas
-    self.oxima=oxima
-    self.ypoloipoLogariasmou=random.uniform(1,5)
-    self.arithmos_dieleysewn=0
+    def __init__(self,kwdkartas,oxima,ypoloipoLogariasmou,arithmos_dieleysewn):
+        self.kwdkartas=kwdkartas
+        self.oxima=oxima
+        self.ypoloipoLogariasmou=random.uniform(1,5)
+        self.arithmos_dieleysewn=0
