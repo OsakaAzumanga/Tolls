1bc654e496a3b589c380b43deb31c74122e8e9ec
StamGl <162900080+StamGl@users.noreply.github.com>
2024-04-16 21:21:52 +0300

    Update Stam_Classes_b.py

--- Stam_Classes_b.py
+++ Stam_Classes_b.py
@@ -2,22 +2,22 @@ import random
 
 class Oxima():
     """ Αυτή είναι η γονική κλάση όλων των οχημάτων"""
-    def __init__(self,arKykloforias,kwdkartas):
+    def __init__(self,arKykloforias):
         self.arKykloforias=arKykloforias
         self.kwdkartas=kwdkartas #Αναφέρομαι στον κωδικό της εκάστοτε ePass
         self.ePass=None
 class Epivatiko(Oxima):
     """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-    def __init__(self,arKykloforias,kwdkartas):
-        super().__init__(arKykloforias,kwdkartas)
+    def __init__(self,arKykloforias):
+        super().__init__(arKykloforias)
     
     def xrewnei(self):
         return 1.50 #Χρέωση διαφορετική σε κάθε όχημα για να υπολογίζεται το εκάστοτε κόστος διέλευσης
 
 class Dikyklo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-        def __init__(self,arKykloforias,kwdkartas):
-              super().__init__(arKykloforias,kwdkartas)
+        def __init__(self,arKykloforias):
+              super().__init__(arKykloforias)
 
         
         def xrewnei(self):
@@ -25,8 +25,8 @@ class Dikyklo(Oxima):
 
 class Fortigo(Oxima):
         """Αυτή η κλάση κληρονομεί όλα τα χαρακτηριστικά της γονικής με την προσθήκη της εξειδικευμένης χρέωσης που απαντάνται μόνο σε αυτήν"""
-        def __init__(self,arKykloforias,kwdkartas):
-            super().__init__(arKykloforias,kwdkartas)
+        def __init__(self,arKykloforias):
+            super().__init__(arKykloforias)
         
         def xrewnei(self):
             return 3.20
